df346002260f20e599e202c014c3d8ae69f0228f
Valentina Marino <valenmarinocol@gmail.com>
2024-01-07 20:56:57 -0500

    main: added mock data for trending items

---
+++ app/data/trendingbr.ts
@@ -0,0 +1,83 @@
+import caraiva from "../../public/images/caraiva.png";
+import conde from "../../public/images/conde.png";
+import corumbau from "../../public/images/corumbau.png";
+import ibitipoca from "../../public/images/ibitipoca.png";
+import lencois from "../../public/images/lencois.png";
+import ilhabela from "../../public/images/lhabela.png";
+import niteroi from "../../public/images/niteroi.png";
+import orla from "../../public/images/caraiva.png";
+import trancoso from "../../public/images/trancoso.png";
+import ubatuba from "../../public/images/ubatuba.png";
+
+export const trending_brasil = [
+  {
+    id: 1,
+    title: "Trancoso",
+    natureza: "mangue / duna / praia",
+    region: "Bahia",
+    img: trancoso,
+  },
+  {
+    id: 2,
+    title: "Ibitipoca",
+    natureza: "cachoeira / rio / serra / gruta",
+    region: "Minas Gerais",
+    img: ibitipoca,
+  },
+  {
+    id: 3,
+    title: "Caraíva",
+    natureza: "praia / mata atlantica",
+    region: "Bahía",
+    img: caraiva,
+  },
+  {
+    id: 4,
+    title: "Orla do Rio de Janeiro",
+    natureza: "areia branca / mar azul",
+    region: "Zona Sul do Rio de Janeiro",
+    img: orla,
+  },
+  {
+    id: 5,
+    title: "Niteroi",
+    natureza: "praia / mata atlantica",
+    region: "Rio de Janeiro",
+    img: niteroi,
+  },
+  {
+    id: 6,
+    title: "IlhaBela",
+    natureza: "mata atlantica / praia / cachoeira",
+    region: "São Paulo, Litoral Norte",
+    img: ilhabela,
+  },
+  {
+    id: 7,
+    title: "Corumbau",
+    natureza: "praia / rio / mangue / arrecife",
+    region: "Bahía",
+    img: corumbau,
+  },
+  {
+    id: 8,
+    title: "Ubatuba",
+    natureza: "mata atlantica / praia / cachoeira",
+    region: "São Paulo",
+    img: ubatuba,
+  },
+  {
+    id: 9,
+    title: "Costa do Conde",
+    natureza: "praia / falecia /rio",
+    region: "Paraiba",
+    img: conde,
+  },
+  {
+    id: 10,
+    title: "Lençóis Marenhenses",
+    natureza: "duna / lagoa / praia",
+    region: "Maranhão",
+    img: lencois,
+  },
+];
